dfad1d204c89b099d3d558d80fbe3e966aa3da9c
Lemuel Flores <is101.lemuel@gmail.com>
2023-03-30 21:11:27 +0800

    fix: use require instead of require_once

--- routes/api/v1/index.php
+++ routes/api/v1/index.php
@@ -1,9 +1,9 @@
 <?php
 
 Route::prefix('/v1')->group(function () {
-    require_once(__DIR__ . '/admin.php');
-    require_once(__DIR__ . '/file.php');
-    require_once(__DIR__ . '/main.php');
-    require_once(__DIR__ . '/order_status.php');
-    require_once(__DIR__ . '/user.php');
+    require __DIR__ . '/admin.php';
+    require __DIR__ . '/file.php';
+    require __DIR__ . '/main.php';
+    require __DIR__ . '/order_status.php';
+    require __DIR__ . '/user.php';
 });
